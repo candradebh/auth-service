371513987614252a4e23d38408e9f4393778ea14
Carlos Martins <carlos.martins@engdb.com.br>
2022-12-07 14:45:30 -0300

    seguranca

--- src/main/java/com/carlosandrade/microservice/auth/WebSecutiryConfigurer.java
+++ src/main/java/com/carlosandrade/microservice/auth/WebSecutiryConfigurer.java
@@ -33,8 +33,11 @@ public class WebSecutiryConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.addFilter(getAuthenticationFilter());
-        //.hasIpAddress(environment.getProperty("gateway.ip"))
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .hasIpAddress(environment.getProperty("gateway.ip"))
+                .and().addFilter(getAuthenticationFilter());
+
         http.headers().frameOptions().disable();
         //http.authorizeRequests().antMatchers(HttpMethod.POST, "/user/**").hasRole("USER").and().addFilter(getAuthenticationFilter());
     }
